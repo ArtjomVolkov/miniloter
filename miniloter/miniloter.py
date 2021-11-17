4d3ddcfd5b24154b56b3ee600b207e92c27a37ed
ASUS <ASUS@DESKTOP-QMOO5M7>
2021-11-17 17:36:49 +0200

    Добавьте файлы проекта.

---
+++ miniloter/miniloter.py
@@ -0,0 +1,17 @@
+﻿from random import *
+F=randint(1,10)
+T=1
+S=int(input("Arvake ära täisarv 1 kuni 10 =>"))
+while S!=F:
+    S=int(input("Palun proovi uuesti =>"))
+    if S<F:
+        print("Teie number on väiksem, kui arvuti ette nägi")
+        T +=1
+    elif S>F:
+        print("Teie number on suurem, kui arvuti ette nägi")
+        T +=1
+    else:
+        print("Õnnitleme, arvasid ära")
+        T +=1
+print("Number oli",S)
+print("Kui palju katseid oli",T)
